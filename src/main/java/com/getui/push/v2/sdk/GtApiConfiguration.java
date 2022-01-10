a87e48ed953e3e45efb66513d8e5a51c4a80b295
liuyj <liuyj@getui.com>
2022-01-10 20:25:13 +0800

    [fix]   1. 修改sdk设置长连接有效期单位

--- src/main/java/com/getui/push/v2/sdk/GtApiConfiguration.java
+++ src/main/java/com/getui/push/v2/sdk/GtApiConfiguration.java
@@ -66,9 +66,10 @@ public class GtApiConfiguration {
      */
     private int maxHttpTryTime = 1;
     /**
-     * 保持长连接的时长，默认10分钟
+     * 保持长连接的时长，默认10s，最大{@link #MAX_KEEP_ALIVE_SECONDS}
      */
-    private long keepAliveMinutes = 10;
+    private long keepAliveSeconds = 10;
+    final long MAX_KEEP_ALIVE_SECONDS = 30;
 
     /**
      * http请求时是否需要信任https
@@ -187,12 +188,16 @@ public class GtApiConfiguration {
         this.maxHttpTryTime = maxHttpTryTime;
     }
 
-    public long getKeepAliveMinutes() {
-        return keepAliveMinutes;
+    public long getKeepAliveSeconds() {
+        return keepAliveSeconds;
     }
 
-    public void setKeepAliveMinutes(long keepAliveMinutes) {
-        this.keepAliveMinutes = keepAliveMinutes;
+    public void setKeepAliveSeconds(long keepAliveSeconds) {
+        if (keepAliveSeconds > MAX_KEEP_ALIVE_SECONDS) {
+            this.keepAliveSeconds = MAX_KEEP_ALIVE_SECONDS;
+        } else {
+            this.keepAliveSeconds = keepAliveSeconds;
+        }
     }
 
     public GtHttpProxyConfig getProxyConfig() {
